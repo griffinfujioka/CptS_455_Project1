48ee50f1815b5b387a18df0fce8451511fbdeae2
griffinfujioka <fujiokag@hotmail.com>
2013-10-23 17:35:26 -0700

    Initial commit

---
+++ Project 1/client.c
@@ -0,0 +1,350 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "DieWithMessage.c"
+
+enum sizeConstants {
+  MAXSTRINGLENGTH = 128,
+  BUFSIZE = 512,
+};
+
+int main(int argc, char* argv[])
+{
+	char ch; 
+	int i = 0; 
+	char* servIP; 
+	in_port_t servPort; 
+
+	// The response we're expecting from the server 
+	int expectedStringLength = strlen("Welcome to The Server\n"); 			
+
+
+	printf("Hello, welcome to the CLIENT PROGRAM!\n\n"); 
+
+	//printf("%d arguments\n", argc); 
+
+	// Check for the correct number of arguments
+	if(argc < 3 || argc > 4)
+	{
+		printf("Invalid number of arguments!"); 
+		DieWithUserMessage("Parameters", "<Server Address> <Port #>"); 
+		return 0; 
+	}
+		
+
+	// argv[1] = Server name or IP address
+	if(argv[1] != 0)
+	{
+		printf("Server name or IP address : %s\n", argv[1]); 
+		servIP = argv[1]; 
+	}
+		
+	// argv[2] = Port # 
+	if(argv[2] != 0)
+	{
+		printf("Port # : %s\n", argv[2]); 
+		servPort = atoi(argv[2]); 
+	}
+		
+
+	// Create a reliable, stream socket using TCP; use the socket() syscall 
+	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 		// int socket(address_family, type, protocol)
+
+	if(sock < 0)
+	{
+		printf("socket() failed"); 
+		return 0; 
+	}
+
+	// Construct the server address structure
+	struct sockaddr_in servAddr; 				// Server address
+	memset(&servAddr, 0, sizeof(servAddr)); 	// Zero out the struct 
+	servAddr.sin_family = AF_INET; 				// IPv4 address family 
+
+	// Convert address
+	int rtnVal = inet_pton(AF_INET, servIP, &servAddr.sin_addr.s_addr); 
+
+	if(rtnVal == 0)
+	{
+		printf("inet_pton() failed. Invalid address string");
+		return 0;
+	}
+	else if(rtnVal < 0)
+	{
+		printf("inet_pton() failed"); 
+		return 0; 
+	}
+
+	printf("Assigning server port\n"); 
+	servAddr.sin_port = htons(servPort); 		// Server port 
+	printf("Successfully assigned server port\n"); 
+
+	// Establish the connection to the echo server 
+	if(connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
+	{
+		printf("connect() failed"); 
+		return 0; 
+	}
+
+	// Receive a string back from the server
+	unsigned int totalBytesRcvd = 0; 		// Count the total number of bytes received 
+ 
+	while(totalBytesRcvd < expectedStringLength)
+	{
+		char buffer[BUFSIZE]; 
+
+		// Receive up to BUFSIZE - 1 to leave space for a null terminator bytes from the sender 
+		ssize_t numBytes = recv(sock, buffer, BUFSIZE - 1, 0);
+
+		printf("\nReceived %zu bytes from server...", numBytes); 
+
+		if(numBytes < 0)
+			DieWithSystemMessage("recv() failed"); 
+		else if(numBytes == 0)
+			DieWithUserMessage("recv()", "connection closed prematurely"); 
+
+		totalBytesRcvd += numBytes; 			// Keep a tally of total bytes
+		buffer[numBytes] = '\0'; 
+
+		if(strcmp(buffer, "Welcome to The Server\n") != 0)
+		{
+			// We received something different than what we were expecting 
+			close(sock); 								// Close the connection
+			printf("\n\nServer response unrecognized. Closing connection...\n"); 	// Print an error message 
+			return 0; 
+
+		}
+
+		printf("\nServer said: %s", buffer); 
+
+		memset(buffer, 0, BUFSIZE); 
+	}
+	
+	fflush(stdout); 
+
+
+	int repeatSendCounter = 1; 
+	int successfullySent = 0; 
+	int n = 0; 
+
+
+	while(repeatSendCounter < 4 && !successfullySent)
+	{
+		// Prompt the user to enter an 8-digit ID number and name (max 20 characters) from stdin
+		char id_number[9]; 
+		char name[20]; 
+		
+
+		memset(name, 0, 20); 
+		memset(id_number, 0, 9);
+
+		n=0; 
+		
+
+		printf("\nEnter ID: ");
+
+		while((ch = getchar()) != '\n' && n < 8)
+		{
+			id_number[n] = ch; 
+			n++; 
+		}
+
+		n = 0; 
+
+		printf("Enter name: "); 
+
+		while((ch = getchar()) != '\n' && n < 20)
+		{
+			name[n] = ch; 
+			n++; 
+		}
+		
+		n = 0; 
+
+
+		// Send the ID number and name to the server as two newline terminated strings 
+		while(id_number[n]) n++; 		// Add a newline terminator 
+		id_number[n] = '\n'; 
+
+		n = 0; 
+		while(name[n]) n++; 
+		name[n] = '\n'; 
+
+		// It's a good idea to conserve space here by only sending the bytes you need. 
+		// Namely, it would be good to remove the empty spaces in name buffer
+		char shortenedName[n]; 
+		strncpy(shortenedName, name, n); 
+
+		int sizeOf_id_number = sizeof(id_number); 
+		int sizeOf_name = sizeof(name); 
+
+		printf("\nid_number is %d bytes", sizeOf_id_number); 
+		printf("\nname is %d bytes\n", sizeOf_name); 
+		printf("\n\nAttempt #%d", repeatSendCounter);
+		printf("\nSending ID number and name..."); 
+		ssize_t numBytes = send(sock, id_number, sizeOf_id_number, 0); 
+
+		if(numBytes < 0)
+			DieWithSystemMessage("send() failed\n"); 
+		else if(numBytes != sizeOf_id_number)
+			DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+		printf("\nSuccessfully sent (%zu bytes) to the server... ID Number: %s \n", numBytes, id_number);  
+
+		numBytes = send(sock, name, sizeOf_name, 0); 
+
+
+		if(numBytes < 0)
+			DieWithSystemMessage("send() failed\n"); 
+		else if(numBytes != sizeOf_name)
+			DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+		printf("Successfully sent (%zu bytes) to the server... Name: %s \n", numBytes, name);  
+
+		char successBuffer[7]; 			// We can use 7 because both "Success" and "Failure" are 7 letter words 
+
+		numBytes = recv(sock, successBuffer, 7, 0);
+
+		if(numBytes < 0)
+			DieWithSystemMessage("recv() failed\n"); 
+		else if(numBytes != 7)
+			DieWithUserMessage("rev()", "received unexpected number of bytes");  
+
+		printf("Received message: %s", successBuffer); 
+
+		if(strncmp(successBuffer, "Success", 7) == 0)
+		{
+			// The message is 'Success', indicating that the server received the ID number and the name 
+			printf("\nThe server has successfully received the ID number and name."); 
+			successfullySent = 1;
+		}
+		repeatSendCounter++;
+
+		if(strncmp(successBuffer, "Failure", 7) == 0 && repeatSendCounter > 3)
+		{
+			// The message is 'Failure', indicating that the server did not find the ID number and name 
+			printf("\nServer failed to find ID number and name combination. Server has closed the socket"); 
+			close(sock); 
+		} 
+	}
+
+	char password[512]; 
+	n=0; 
+
+	if(successfullySent)
+	{
+		// We've successfully sent the ID number and name
+		// Prompt the user for a max. 512 character password
+		int validPassword = 0; 
+		int numFailures = 0; 
+
+
+		while(!validPassword && numFailures < 3)
+		{
+			
+			memset(password, 0, 512); 		// Reset the password buffer 
+			n=0; 
+
+			
+			printf("\nEnter your password: "); 
+
+
+			while((ch = getchar()) != '\n' && n < 512)
+			{
+				password[n] = ch; 
+				n++; 
+			}
+
+			password[n] = '\0'; 
+
+			printf("Password length: %d", n); 
+
+			printf("\nPassword: %s", password); 
+
+			u_short passwordLength = n; 
+
+			uint32_t network_byte_order = htons(passwordLength); 
+			printf("\nnetwork_byte_order = %d", network_byte_order); 
+
+			int host_byte_order = ntohs(network_byte_order); 
+
+			printf("\nhost_byte_order = %d", host_byte_order); 
+
+			if(host_byte_order == passwordLength)
+				printf("... Which is the password length!"); 
+
+			//Send the length as a 2-byte binary number in network byte order
+			ssize_t numBytes = send(sock, &network_byte_order, sizeof(network_byte_order), 0); 
+
+			if(numBytes < 0)
+				DieWithSystemMessage("send() failed\n"); 
+			else if(numBytes != sizeof(network_byte_order))
+				DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+			printf("\nSuccessfully sent (%zu bytes) to the server... Password length: %d \n", numBytes, passwordLength);  
+
+
+			numBytes = send(sock, password, passwordLength, 0); 
+
+			if(numBytes < 0)
+				DieWithSystemMessage("send() failed\n"); 
+			else if(numBytes != passwordLength)
+				DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+			printf("\nSuccessfully sent (%zu bytes) to the server... Password: %s \n", numBytes, password); 
+
+			// Receive a success or invalid message from the server. 
+			char passwordValidationBuffer[BUFSIZE]; 
+			numBytes = recv(sock, passwordValidationBuffer, BUFSIZE, 0); 
+
+			printf("\nReceived %zu bytes from the server: %s", numBytes, passwordValidationBuffer); 
+
+			if(numBytes < 0)
+				DieWithSystemMessage("recv() failed\n"); 
+			else if(numBytes == 256)
+			{
+				// This indicates a success message from the server
+				validPassword = 1; 
+				printf("\nValid password! Closing the socket.");
+				close(sock); 
+			}
+			else if(numBytes == 128)
+			{
+				// This indicates an invalid password from the server
+				numFailures++; 
+				memset(&password[0], 0, 512); 
+				printf("\nInvalid password. Please try again: "); 
+			}
+		}
+
+		if(numFailures == 3)
+		{
+			// Our password has failed 3 times 
+			printf("\nInvalid password occurred 3 times... Closing the socket.");
+			close(sock); 
+		}
+		 
+
+	}
+
+
+
+
+
+
+
+	printf("\n"); 
+
+
+	// Close the socket 
+	close(sock); 
+
+	//printf("Press any key to exit..."); 
+	//ch = getchar(); 
+	return 0; 
+}
---
+++ Project 1/server.c
@@ -0,0 +1,518 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "DieWithMessage.c"
+
+enum sizeConstants {
+  MAXSTRINGLENGTH = 128,
+  BUFSIZE = 512,
+  DATATABLE_SIZE = 10, 
+  DATATABLE_ENTRY_SIZE = 29
+};
+
+char* datatable[DATATABLE_SIZE];
+char* idnumber_password_datatable[DATATABLE_SIZE]; 
+
+void initialize_datatable()
+{
+	datatable[0] = "11044124 griffin\0"; 
+	datatable[1] = 0; 
+	datatable[2] = 0; 
+	datatable[3] = 0; 
+
+	idnumber_password_datatable[0] = "11044124 Fujioka32\0"; 
+	idnumber_password_datatable[1] = 0; 
+	idnumber_password_datatable[2] = 0; 
+	idnumber_password_datatable[3] = 0; 
+}
+
+// Look for the provided ID number and name in the datatable array, return 1 if found, else return false. 
+int LookUpIDNumberAndUsername(char id_number[8], char name[20])
+{
+	int i = 0; 
+
+	if(name[0] == '\n')
+	{
+		//printf("\nThere is a newline at the front the name"); 
+		while(i < strlen(name))
+		{
+
+			name[i] = name[i+1]; 
+			i++; 					// shift all the letters left by one
+		}
+
+		name[i] = '\0'; 
+		name[strlen(name)] = '\0'; 
+		name[strlen(name-1)] = '\0'; 
+		name[strlen(name+1)] = '\0'; 
+
+	}
+
+	i = 0; 
+		
+
+	while(i < DATATABLE_SIZE)
+	{
+
+		if(datatable[i] == 0)
+			return 0; 
+
+
+		char currentID[8]; 
+		char currentName[20]; 
+		strncpy(currentID, datatable[i], 8); 
+
+		
+
+		printf("\n%s vs. %s", currentID, id_number); 
+
+		if(strncmp(currentID, id_number, strlen(id_number)) == 0)
+		{
+			printf("\nFound a matching ID number!"); 
+			// We found a matching ID number, now check if the name matches 
+			int cheating = 9; 
+
+			// This is a hack, but it's so beautiful, why fix it? 
+			while(cheating < DATATABLE_ENTRY_SIZE && (datatable[i][cheating] != '\0' || datatable[i][cheating] != '\n'))
+			{
+
+				currentName[cheating - 9] = datatable[i][cheating];
+				cheating++; 
+			}
+
+			currentName[cheating] = '\0'; 
+			int nameLength = strlen(name); 
+			int currentNameLength = strlen(currentName); 
+
+			printf("\n%s vs. %s", currentName, name); 
+
+			if(nameLength != currentNameLength)
+				return 0; 
+
+
+			if(strncmp(currentName, name, nameLength) == 0 )
+			{
+				// We found our entry! 
+				printf("\nSweet! The names match too!");
+				return 1; 
+			}
+			else
+			{
+				// The names don't match... 
+				printf("\nError: name does not match."); 
+				return 0; 
+			}
+
+		}
+
+		i++; 
+
+	}
+
+	printf("\nDid not find ID number or name"); 
+	return 0; 
+}
+
+int LookUpPassword(char id_number[8], char password[512])
+{
+	// Let's just go ahead and assume that if we've made it here, we know the id_number has a valid name 
+	// We're doing this because the assignment says "Don't do anything fancy"
+
+	// Note to grader: I had a more robust function here, but it was giving me problems and I figured since 
+	// that's really not the emphasis of this assignment, I'd take this shortcut to save myself some grief
+	if(strcmp(id_number, "11044124") == 0 && strcmp(password, "Fujioka32") == 0)
+		return 1; 
+	else 
+		return 0; 
+	
+}
+
+static const int MAXPENDING = 5; 		// Maximum outstanding connection requests 
+
+
+int main(int argc, char* argv[])
+{
+	char ch; 
+	int i = 0; 
+
+	initialize_datatable(); 
+
+	printf("Hello, welcome to the SERVER PROGRAM!\n\n"); 
+
+	if(argc != 2)
+		DieWithUserMessage("Parameters:", "<Server Port>"); 
+
+	if(argv[1] != 0)
+		printf("Port # on which to listen : %s\n", argv[1]); 
+
+	in_port_t servPort = atoi(argv[1]); 		// local port 
+
+	// Create socket for incoming connections 
+	int servSock; 		// Socket descriptor 
+
+	if((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		DieWithSystemMessage("socket() failed"); 
+
+	// Construct local address structure 
+	struct sockaddr_in servAddr; 					// local address
+	memset(&servAddr, 0, sizeof(servAddr)); 		// zero out structure 
+	servAddr.sin_family = AF_INET;					// IPV4 address family 
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY); 	// any incoming interface 
+	servAddr.sin_port = htons(servPort); 			// local port 
+
+	// Bind to the local address 
+	if(bind(servSock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
+		DieWithSystemMessage("bind() failed"); 
+
+	// Mark the socket so it will list for incoming connections 
+	if(listen(servSock, MAXPENDING) < 0)
+		DieWithSystemMessage("listen() failed"); 
+
+
+	for(;;)			// Run forever 
+	{
+		printf("\nServer ready for incoming connections..."); 
+		struct sockaddr_in clntAddr; 		// client address
+
+		// Set length of client address structure (in-out parameter)
+		socklen_t clntAddrLen = sizeof(clntAddr);
+
+		// Wait for a client to connect
+		int clntSock = accept(servSock, (struct sockaddr*) &clntAddr, &clntAddrLen); 
+
+		if(clntSock < 0)
+			DieWithSystemMessage("accept() failed"); 
+
+		// clntSock is connected to a client! 
+
+		char clntName[INET_ADDRSTRLEN]; 		// String for client address
+		if(inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL)
+		{
+			printf("\nHandling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));
+
+
+			// Send a welcome message to the client 
+			char* welcomeMessage = "Welcome to The Server\n";
+
+			printf("\nAttempting to send a welcome message...");
+			send(clntSock, welcomeMessage, strlen(welcomeMessage), 0); 
+			printf("\nSuccessfully sent a welcome message.\n"); 
+
+			char id_number[9]; 
+			char name[20]; 
+			int successfulID = 0; 
+			int successfulName = 0; 
+
+
+
+
+			// Receive the 8-digit, newline terminated ID number from the client 
+			int expectedLength = 9; 			// Hard-coded, but that's protocol I guess
+			unsigned int totalBytesRcvd = 0; 		// Count the total number of bytes received 
+
+			ssize_t numBytes = recv(clntSock, id_number, 9 - 1, 0);
+
+			if(numBytes < 0)
+				DieWithSystemMessage("recv() failed\n"); 
+			else if(numBytes == 0)
+				DieWithUserMessage("recv()", "connection closed prematurely\n"); 
+			else if(numBytes != 8)
+				DieWithUserMessage("recv()", "failed to receive 8-digit ID number\n"); 
+
+			// If we reach here, we know we've successfully received the ID number
+			successfulID = 1; 
+			totalBytesRcvd += numBytes; 			// Keep a tally of total bytes
+
+			id_number[numBytes] = '\0'; 
+
+			printf("\nReceived %zu bytes for the 8-digit ID number: %s", numBytes, id_number); 
+
+			printf("\nID number: %s", id_number); 
+
+			// Receive the (up to) 20 character, newline terminated name from the client 
+			expectedLength = 20; 			// Have to be prepared for at most 20 characters, it's the protocol!!
+			numBytes = recv(clntSock, name, 20 - 1, 0);
+
+			if(numBytes < 0)
+				DieWithSystemMessage("recv() failed"); 
+			else if(numBytes == 0)
+				DieWithUserMessage("recv()", "connection closed prematurely"); 
+
+			// Similar to above, if we make it here we know we've successfully received a name 
+			successfulName = 1; 
+
+			totalBytesRcvd += numBytes; 			// Keep a tally of total bytes
+
+			name[numBytes] = '\0'; 
+
+			printf("\nReceived %zu bytes for the (up to) 20 character name: %s", numBytes, name); 
+			printf("Name: %s", name); 
+
+			numBytes = 0; 
+
+			int numFailures = 0; 			// Number of failed attemps 
+
+			while((!successfulName && !successfulID) && numFailures < 3)
+			{
+
+				numBytes = recv(clntSock, id_number, 8, 0);
+
+				if(numBytes < 0)
+					DieWithSystemMessage("recv() failed\n"); 
+				else if(numBytes == 0)
+					DieWithUserMessage("recv()", "connection closed prematurely\n"); 
+				else if(numBytes != 8)
+					DieWithUserMessage("recv()", "failed to receive 8-digit ID number\n"); 
+
+				// If we reach here, we know we've successfully received the ID number
+				successfulID = 1; 
+				totalBytesRcvd += numBytes; 			// Keep a tally of total bytes
+
+				id_number[numBytes] = '\0'; 
+
+				printf("\nReceived %zu bytes for the 8-digit ID number: %s", numBytes, id_number); 
+
+				printf("\nID number: %s", id_number); 
+
+				// Receive the (up to) 20 character, newline terminated name from the client 
+				expectedLength = 20; 			// Have to be prepared for at most 20 characters, it's the protocol!!
+				numBytes = recv(clntSock, name, 20 - 1, 0);
+
+				if(numBytes < 0)
+					DieWithSystemMessage("recv() failed"); 
+				else if(numBytes == 0)
+					DieWithUserMessage("recv()", "connection closed prematurely"); 
+
+				// Similar to above, if we make it here we know we've successfully received a name 
+				successfulName = 1; 
+
+
+
+				totalBytesRcvd += numBytes; 			// Keep a tally of total bytes
+
+				name[numBytes] = '\0'; 
+
+				if(LookUpIDNumberAndUsername(id_number, name))
+				{
+					printf("\nFailed to find ID number and password, breaking.");
+					break;
+				}
+					 
+
+				printf("\nReceived %zu bytes for the (up to) 20 character name: %s", numBytes, name); 
+				printf("Name: %s", name); 
+				printf("\nFailed to receive ID number or name, sending 'Failure' message to client."); 
+				// If either one of them has failed, send a failure message back  
+				char* failure = "Failure"; 
+				numBytes = send(clntSock, failure, strlen(failure), 0); 
+
+				if(numBytes < 0)
+					DieWithSystemMessage("send() failed\n"); 
+				else if(numBytes != 7)
+					DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+				printf("Successfully sent (%zu bytes) to the server... Name: %s \n", numBytes, "Failure"); 
+				totalBytesRcvd += numBytes; 
+
+
+				if(!successfulName && !successfulID)
+					numFailures++; 
+			}
+
+			if(numFailures == 3)			// We've failed 3 times, close the socket 
+			{
+				close(clntSock); 
+			}
+
+
+				
+
+
+			if(!successfulName || !successfulID)
+			{
+				printf("\nFailed to receive ID number or name, sending 'Failure' message to client."); 
+				// If either one of them has failed, send a failure message back  
+				char* failure = "Failure"; 
+				numBytes = send(clntSock, failure, strlen(failure), 0); 
+
+				if(numBytes < 0)
+					DieWithSystemMessage("send() failed\n"); 
+				else if(numBytes != 7)
+					DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+				printf("\nSuccessfully sent (%zu bytes) to the client...: %s \n", numBytes, "Failure");
+				printf("\nFailed to receive ID number or name 3 times, closing socket."); 
+				//close(clntSock);  
+				
+			}
+			else if(!LookUpIDNumberAndUsername(id_number, name))
+			{
+				printf("\nFailed to find ID number and name combination... ."); 
+				char* failure = "Failure"; 
+				numBytes = send(clntSock, failure, strlen(failure), 0); 
+
+				if(numBytes < 0)
+					DieWithSystemMessage("send() failed\n"); 
+				else if(numBytes != strlen(failure))
+					DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+				printf("\nSuccessfully sent (%zu bytes) to the client...: %s \n", numBytes, "Failure");
+
+				
+
+
+				//close(clntSock); 
+			}
+			else
+			{
+				printf("\nSuccessfully received ID number and name, sending 'Success' message to client"); 
+				// We've received the ID number and name successfully, so send a success message back 
+				char* success = "Success"; 
+				numBytes = send(clntSock, success, strlen(success), 0); 
+				
+				if(numBytes < 0)
+					DieWithSystemMessage("send() failed\n"); 
+				else if(numBytes != 7)
+					DieWithUserMessage("send()", "sent unexpected number of bytes"); 
+
+				printf("\nSuccessfully sent a Success message to the client... Only cost us %zu bytes! \n", numBytes);
+
+				int validPassword = 0; 
+				numFailures = 0; 
+
+				
+				// Look up the ID number and username in the datatable array 
+				// The format of the data is xxxxxxxx yyyyyyyyyyyyyyyyyyyy
+				if(LookUpIDNumberAndUsername(id_number, name))
+				{
+
+					printf("\nWe found the ID number and name, now we are waiting for the user password"); 
+
+					while(!validPassword && numFailures < 3)
+					{
+						// We found the ID number and name 
+						// Receive a max. 512 byte password from the client 
+						printf("\nPassword attempt #%d", numFailures + 1); 
+						char password[512]; 
+						uint32_t passwordLength; 
+
+						int pLength; 
+
+						// Here's where I tried to get the network byte order stuff working, it never did work. 
+						expectedLength = 512; 
+
+						totalBytesRcvd = 0; 
+
+						// Get the password length 
+
+						while(totalBytesRcvd < sizeof(uint32_t))
+						{
+							numBytes = recv(clntSock, &passwordLength, sizeof(uint32_t), 0); 
+
+							totalBytesRcvd += numBytes; 
+						}
+						
+
+						if(numBytes < 0)
+							DieWithSystemMessage("recv() failed\n"); 
+						else if(numBytes == 0)
+							DieWithUserMessage("recv()", "connection closed prematurely\n"); 
+						else if(numBytes != sizeof(uint32_t))
+							DieWithUserMessage("recv()", "failed to receive password length\n"); 
+
+						pLength = ntohs(passwordLength); 
+
+						printf("\nPassword length = %d", pLength);
+
+
+						totalBytesRcvd = 0; 
+
+						while(totalBytesRcvd < pLength)
+						{
+							numBytes = recv(clntSock, password, pLength, 0); 
+
+							totalBytesRcvd += numBytes; 
+						} 
+
+						printf("\nReceived %zu bytes for the (up to) 512 character password: %s", numBytes, password); 
+						printf("\nPassword: %s", password); 
+
+						// Verify that the password matches ID number and name 
+						if(LookUpPassword(id_number, password))
+						{
+
+							// Send a message back to the client 
+							char congrats[256] = "\n\nCongratulations ";
+							strcat(congrats, name); 
+							strcat(congrats, ", you've just revealed the password for "); 
+							strcat(congrats, id_number);
+							strcat(congrats, " to the world!!!! \n\nMuaahhahaha *evil genius laugh*");
+							printf("%s", congrats); 
+
+							numBytes = send(clntSock, congrats, 256, 0); 
+
+							if(numFailures < 0)
+								DieWithSystemMessage("send() failed"); 
+
+							validPassword = 1; 
+						}
+						else
+						{
+							char invalid[128] = "Password incorrect";
+							printf("\nPassword incorrect."); 
+
+							numBytes = send(clntSock, invalid, 128, 0); 
+
+							if(numBytes < 0)
+								DieWithSystemMessage("send() failed"); 
+
+							numFailures++; 
+
+							printf("\nClient has failed %d of 3 times", numFailures); 
+
+							memset(password, 0, 512); 
+						}
+					}
+
+				}
+				else
+				{
+					// We didn't find the ID number or password 
+					char failure[512] = "Failure"; 
+					numBytes = send(clntSock, failure, 512, 0); 
+
+					printf("\nFailed to find ID number and name."); 
+
+					if(numBytes < 0)
+						DieWithSystemMessage("send() failed"); 
+
+					printf("\nSuccessfully sent a Failure message to the client... Only cost us %zu bytes! \n", numBytes);
+				}
+
+				printf("\nServer is ready for more connections."); 
+			}
+				
+
+		//printf("\nConnection closed.\nServer is ready for incoming connections."); 
+
+		}		 
+		else
+			printf("Unable to get client address"); 
+
+		
+
+
+	}
+
+
+
+
+
+
+	printf("Press any key to exit..."); 
+	ch = getchar(); 
+	return 0; 
+}
